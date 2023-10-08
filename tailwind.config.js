57253063e9c63a464339d92c4b0648da9bc8dac1
Abolfazl Rasoulizade <abolfazlrasoulizade@gmail.com>
2023-10-08 18:49:47 +0330

    feat: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// module.exports = {
+//   future: {
+//     // removeDeprecatedGapUtilities: true,
+//     // purgeLayersByDefault: true,
+//   },
+//   purge: [],
+//   theme: {
+//     extend: {},
+//   },
+//   variants: {},
+//   plugins: [],
+// }
+
+import {tailwindConfig} from '@alwatr/style';
+
+tailwindConfig.content = ['content/**/*'];
+
+export default tailwindConfig;
