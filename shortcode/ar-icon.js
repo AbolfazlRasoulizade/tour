4898ee70ade10f0565af7acb475c942e6febcc26
Abolfazl Rasoulizade <abolfazlrasoulizade@gmail.com>
2023-10-16 20:25:08 +0330

    feat(shortcode): make aricon

---
+++ shortcode/ar-icon.js
@@ -0,0 +1,11 @@
+const {readFile} = require('node:fs/promises');
+
+async function arIcon(iconName, customClass = '') {
+  let icon;
+
+  icon = await readFile(`node_modules/@alwatr/icon-set-material/svg/outline/${iconName}.svg`, 'utf8');
+
+  return `<div class="text-blue-500 h-4 w-4 box-content align-middle self-center grow-0 shrink-0 [contain:size_layout_paint_style] [&>svg]:block [&>svg]:h-full [&>svg]:w-full [&>svg]:fill-current ${customClass}">${icon}</div>`;
+}
+
+module.exports = {arIcon};
