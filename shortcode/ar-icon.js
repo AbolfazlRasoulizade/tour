63f5b7f7e8b41f9f44fe27e53d831b896c72202d
Abolfazl Rasoulizade <abolfazlrasoulizade@gmail.com>
2023-10-18 22:12:17 +0330

    feat: come to new design

--- shortcode/ar-icon.js
+++ shortcode/ar-icon.js
@@ -5,7 +5,7 @@ async function arIcon(iconName, customClass = '') {
 
   icon = await readFile(`node_modules/@alwatr/icon-set-material/svg/outline/${iconName}.svg`, 'utf8');
 
-  return `<div class="text-blue-500 h-4 w-4 box-content align-middle self-center grow-0 shrink-0 [contain:size_layout_paint_style] [&>svg]:block [&>svg]:h-full [&>svg]:w-full [&>svg]:fill-current ${customClass}">${icon}</div>`;
+  return `<div class="text-blue-500 h-6 w-6 box-content align-middle self-center grow-0 shrink-0 [contain:size_layout_paint_style] [&>svg]:block [&>svg]:h-full [&>svg]:w-full [&>svg]:fill-current ${customClass}">${icon}</div>`;
 }
 
 module.exports = {arIcon};
--- site/_ts/main.ts
+++ site/_ts/main.ts
@@ -1,7 +1,7 @@
 import alpine from 'alpinejs';
 
 import './lib/banner.js';
-import './lib/data.product-item.js';
+// import './lib/data.product-item.js';
 import './lib/global-util.js';
 import {logger} from './lib/logger.js';
 import './lib/register-service-worker.js';
